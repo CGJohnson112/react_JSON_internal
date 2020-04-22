93b51652cdeed423cc65d02cb8f70e2d651d9dd0
Chris Johnson <chrisjohnson112@yahoo.com>
2020-04-22 03:23:28 -0500

    Added JS, for loop and ratings count, ratings average, DOM display in browswer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
+//import ReactDOM from 'react-dom'
 import Data from './ratings.json'
 
-export class App extends Component {
+//console.log(Data);
+
+let ratingsSum = 0;
+let itemsFound = 0;
+const len = Data.length;
+let item = null;
+for (let i = 0; i < len; i++) {
+  item = Data[i];
+  
+
+  
+    ratingsSum = item.rating + ratingsSum;
+    console.log("This is", ratingsSum);
+    itemsFound = itemsFound + 1;
+ 
+}
+
+const avgCount = ratingsSum / itemsFound;
+console.log("Avg count:", avgCount);
+
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      average: avgCount,
+    };
+  }
+
   render() {
     return (
       <div className="App">
+      <div id="avgCount">
+        <h4>Average rating: {this.state.average} </h4>
+      </div>
         { Data.map(post => {
             return(
               <div className="farra" key={post.id}>
